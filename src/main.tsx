b8fa11aa318129a1e2adc59095eaef69c6b44621
barbara michaud <barbara.michaud@outlook.fr>
2025-09-18 15:47:31 +0200

    adding hash route

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter, Route, Routes} from "react-router";
+import {Route, Routes} from "react-router";
+import { HashRouter } from 'react-router-dom';
 import GraphPage from "@/screens/GraphPage.tsx";
 import CreateEditScenarioPage from "@/screens/CreateEditScenarioPage.tsx";
 import SimulationPage from "@/screens/SimulationPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
             <Route path="/" element={<App />} />
               <Route path="/scenario/create" element={<CreateEditScenarioPage />} />
@@ -17,6 +18,6 @@ createRoot(document.getElementById('root')!).render(
             <Route path="/graphs" element={<GraphPage />} />
               <Route path="/simulation/:id" element={<SimulationPage />} />
           </Routes>
-      </BrowserRouter>
+       </HashRouter>
   </StrictMode>,
 )
